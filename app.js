a834a0c8c67016254a98144cd822260100b27b9c
abrown4123 <abrown4123@gmail.com>
2017-04-30 18:32:57 +0000

    Setup register and login routes that allow access to Secret page

---
+++ app.js
@@ -0,0 +1,72 @@
+var express               = require("express"),
+    app                   = express(),
+    mongoose              = require("mongoose"),
+    User                  = require("./models/user"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    localStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+    
+mongoose.connect("mongodb://localhost/authDemo");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require("express-session")({
+    secret: "password",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//==========
+// ROUTES
+//==========
+app.get("/", function(req, res){
+   res.render("home"); 
+});
+
+app.get("/secret", function(req, res){
+   res.render("secret"); 
+});
+
+//==============
+// AUTH ROUTES
+//==============
+
+//Register
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          res.render("register");
+      }
+          passport.authenticate("local")(req, res, function(){
+             res.redirect("/secret"); 
+         });
+   }); 
+});
+
+//Login
+app.get("/login", function(req,res){
+   res.render("login"); 
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server is Running"); 
+});
